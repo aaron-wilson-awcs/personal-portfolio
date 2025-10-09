4ea4dd7aca1b3711d54106ef3add721e6911c0ba
aaron-wilson-awcs <aaron.wilson@remarkable.net>
2025-10-09 17:55:47 +0100

    added background cover image and avata image

--- PersonalPortfolio.React/src/main.tsx
+++ PersonalPortfolio.React/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Header from './Header.tsx'
+import Header from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
