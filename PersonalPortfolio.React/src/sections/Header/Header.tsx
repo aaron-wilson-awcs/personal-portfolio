ead13a6c927104a97bfdfafbf6a63e80c4c76e39
aaron-wilson-awcs <aaron.wilson@remarkable.net>
2025-10-15 16:29:38 +0100

    fixed scroll issue

--- PersonalPortfolio.React/src/sections/Header/Header.tsx
+++ PersonalPortfolio.React/src/sections/Header/Header.tsx
@@ -4,12 +4,12 @@ function Header() {
   return (
     <>
       <div className='bar-colour-grey h-16'>
-        <h1 className='text-center'>AW Code Solutions Ltd.</h1>
+        <h1 className='text-center'>Personal Portfolio</h1>
       </div>
 
-      <div>
+      <div className=''>
         <div id='cover-image'>
-          <img src="/src/assets/mountain-snowboarding.jpeg" alt="cover-image" className="top-bottom-border w-full absolute left-0 h-2/5 mb-52"></img>
+          <img src="/src/assets/mountain-snowboarding.jpeg" alt="cover-image" className="absolute top-bottom-border w-full overflow-x-hidden left-0 h-2/5 mb-52"></img>
         </div>
         <div className='w-full'>
           <div className="flex items-center justify-center relative z-10 mb-2.5">
